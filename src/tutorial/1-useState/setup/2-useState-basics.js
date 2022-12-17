4a8c9832d8b3b36bae1669fc6377303ad0c76042
Sahib <sahib.arora@uberflip.com>
2022-12-17 00:57:07 -0500

    useState - finished

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,45 @@
 import React, { useState } from 'react';
 
+// useState is state hook
+// all hooks start with `use` also apply to hooks we create
+// Component must be uppercase if we use a hook
+// hook must be within component/function body
+// cannot call conditionally - not the function useState(1)[1]
+// basically for above point useState cannot be in if, it is basically unique to a function or component 
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('Hello World!'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  // We can have multiple useState if we want...
+  const [text, setText] = useState('Random Title');
+  const [btnText, setBtnText] = useState('I am button text');
+
+  // Basically in useState - first value is the value of the variable at useState(1)[0]
+  // second is the function which holds power to change that value useState(1)[1] - function
+  const handleClick = () => {
+    if(text !== 'Hello World!'){
+      setText('Hello World!');
+    }else {
+      setText('Random Title!');
+    }
+
+    if(btnText !== 'I am button text'){
+      setBtnText('I am button text');
+    }else{
+      setBtnText('Button text changed');
+    }
+    
+  };
+
+  return (
+    <div>
+      <h1>{text}</h1>
+      <button type='button' className='btn' onClick={handleClick}>{btnText}</button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,54 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState([{
+    id: 1,
+    name: 'Peter', 
+    age: 24, 
+    message:'random Message'
+  }, {
+    id: 2,
+    name: 'John', 
+    age: 26, 
+    message:'random Message - 2'
+  }]);
+
+  const changeMessage = (id = null) => {
+    if(!id){
+      if(person.message === 'random Message'){
+        setPerson({...person, message: 'Hello World!'});
+      }else {
+        setPerson({...person, message: 'random Message'});
+      }
+    }else {
+      var newPerson = person.map((p, index) => {
+        if(p.id !== id){
+          return p;
+        }else {
+          if(p.message === 'random Message'){
+            p.message = 'random Message - 2';
+          }else{
+            p.message = 'random Message';
+          }
+          return p;
+        }
+      });
+      setPerson(newPerson);
+    }
+  }
+
+  return <>
+    {
+      person.map((p) => {
+        return <div key={p.id}>
+          <h3>{p.name}</h3>
+          <h3>{p.age}</h3>
+          <h3>{p.message}</h3>
+          <button tyep='button' className='btn' onClick={() => changeMessage(p.id)}>change message</button>
+        </div>
+      })
+    }
+  </>;
 };
 
 export default UseStateObject;
