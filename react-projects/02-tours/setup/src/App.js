2b21bec5872dc8791afa9d9fd4c8e5db8583d853
Sahib <sahib.arora@uberflip.com>
2022-12-23 00:39:47 -0500

    Finished 3 section-5 react-projets

--- react-projects/02-tours/setup/src/App.js
+++ react-projects/02-tours/setup/src/App.js
@@ -5,7 +5,54 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  const fetchProjects = async() => {
+    setLoading(true);
+    try{
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setTours(data);
+    }catch(e){
+      setLoading(false);
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  if(loading) {
+    return (
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+
+  if(tours.length === 0){
+    return ( 
+      <main>
+        <h2>no tours left</h2>
+        <button onClick={fetchProjects} className='btn'>Refresh</button>
+      </main>
+    )
+  }
+    
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  )
 }
 
 export default App
--- react-projects/02-tours/setup/src/Tours.js
+++ react-projects/02-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = (props) => {
+  return <>
+    <section>
+      <div className='title'>
+        <h2>ours tour</h2>
+        <div className='underline'></div>
+        <div>
+        {props.tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={props.removeTour}></Tour>
+        })}
+        </div>
+      </div>
+    </section>
+  </>;
 };
 
 export default Tours;
