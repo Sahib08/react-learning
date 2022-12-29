e1588eab357ad3bb260505d416d1910bd361e375
Sahib <sahib.arora@uberflip.com>
2022-12-29 00:14:24 -0500

    Forms - controlled inputs done

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,59 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(firstName.trim() && email.trim()){
+      const person = { id: new Date().getTime().toString(), firstName, email};
+      setPeople((prevValue) => {
+        return [...prevValue, person];
+      });
+      setFirstName('');
+      setEmail('');
+      console.log(people);
+    }
+  }
+
+  // We connect our inputs with the state values.
+  return <>
+    <article>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='firstName'>Name: </label>
+          <input 
+          type='text' 
+          id='firstName' 
+          name='firstName' 
+          value={firstName}
+          onChange={(e)=>setFirstName(e.target.value)}
+          ></input>
+        </div>
+        <div className='form-control'>
+          <label htmlFor='email'>Email: </label>
+          <input 
+          type='text' 
+          id='email' 
+          name='email' 
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          ></input>
+        </div>
+        <button type='submit'>Add Person</button>
+      </form>
+      {people.map((person)=>{
+        return (
+          <div className='item' key={person.id}>
+            <h4>{person.firstName}</h4>
+            <p>{person.email}</p>
+          </div>
+        )
+      })}
+    </article>
+  </>;
 };
 
 export default ControlledInputs;
